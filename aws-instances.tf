2eeb98d9e697d9f53ad2c68143ebdacbaf8aef49
Glenn Gillen <me@glenngillen.com>
2019-11-01 19:00:29 +1100

    Have a policy.

--- aws-instances.tf
+++ aws-instances.tf
@@ -7,3 +7,28 @@ resource "aws_s3_bucket" "bucket-1" {
   acl    = "private"
 }
 
+resource "aws_s3_bucket_policy" "policy" {
+  bucket = "${aws_s3_bucket.bucket-1.id}"
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "DenyNonSecureTraffic",
+      "Action": "s3:*",
+      "Effect": "Deny",
+      "Principal":  "*",
+      "Resource": [ 
+        "arn:aws:s3:::${aws_s3_bucket.bucket-1.bucket}/*",
+        "arn:aws:s3:::${aws_s3_bucket.bucket-1.bucket}" 
+      ],
+      "Condition": {
+        "Bool": {
+          "aws:SecureTransport": "false"
+        }
+      }
+    }
+  ]
+}
+POLICY
+}
